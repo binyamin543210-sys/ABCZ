82e6338df4bb93a0b46c7a850799e60644f8d8d5
binyamin543210-sys <binyamin543210@gmail.com>
2026-01-02 15:01:32 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -50,3 +50,29 @@ self.addEventListener('fetch', (event) => {
     }
   })());
 });
+
+
+
+// ===============================
+// PUSH NOTIFICATIONS
+// ===============================
+self.addEventListener("push", (event) => {
+  const data = event.data ? event.data.json() : {};
+
+  self.registration.showNotification(
+    data.title || "BNAPP",
+    {
+      body: data.body || "יש תזכורת חדשה",
+      icon: "/icon-192.png",
+      badge: "/icon-192.png",
+      data: data.url || "/"
+    }
+  );
+});
+
+self.addEventListener("notificationclick", (event) => {
+  event.notification.close();
+  event.waitUntil(
+    clients.openWindow(event.notification.data)
+  );
+});
