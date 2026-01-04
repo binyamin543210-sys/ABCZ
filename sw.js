92ccea9bd62067c208b2d125efc8d7ff38b623e8
binyamin543210-sys <binyamin543210@gmail.com>
2026-01-04 17:21:04 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,19 @@
+importScripts("https://www.gstatic.com/firebasejs/9.22.2/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/9.22.2/firebase-messaging-compat.js");
+
+firebase.initializeApp({
+  apiKey: "AIzaSyCa808qwjJ8bayhjkTqZ8P9fRhfgi19xtY",
+  authDomain: "bnapp-ddcbf.firebaseapp.com",
+  projectId: "bnapp-ddcbf",
+  messagingSenderId: "523128255450",
+  appId: "1:523128255450:web:d29cdda3f21435f96686e3"
+});
+
+const messaging = firebase.messaging();
+
+
+
+
 const CACHE_NAME = 'bnapp-v3-5-2';
 
 self.addEventListener('install', (event) => {
